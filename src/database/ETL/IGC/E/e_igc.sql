23b96e3e5f34381d38f4b4b1e746387eb21f6b38
kenneth <254309@via.dk>
2018-05-17 11:38:49 +0200

    waiting for igc dao

--- src/database/ETL/IGC/E/e_igc.sql
+++ src/database/ETL/IGC/E/e_igc.sql
@@ -1,5 +1,27 @@
 
-select * from user_tab_columns where table_name = IGC_Source_Data;
-select * from user_tab_columns where table_name = data_logger;
+-- select column_name || ', ' from user_tab_columns where table_name like '%IGC_SOURCE_DATA%';
+-- select column_name || ', ' from user_tab_columns where table_name like '%DATA_LOGGER%';
+
+
+insert into FULLY_EXTRACTED_IGC
+  (select
+  igc.ID,
+  igc.RECORDTYPE,
+  igc.TIMEOFLOG,
+  igc.LATITUDE,
+  igc.LONGITUDE,
+  igc.SATELITECOVERAGE,
+  igc.PRESSUREALTITUDE,
+  igc.GPSALTITUDE,
+  igc.FLIGTHID,
+  log.FLIGHTDATE
+  from IGC_SOURCE_DATA igc
+    join data_logger log ON
+      igc.FLIGTHID = log.ID
+  where log.ID not in (Select id from D_FLIGHT) -- this makes sure that we only extract the new ones
+  )
+
+;
+
 
 
