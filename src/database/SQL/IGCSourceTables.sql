d00794a60e2fdcf31130e64413a121eb1def90e5
Nikolina <starfear@abv.bg>
2018-05-17 09:54:12 +0200

    Created IGCSourceTables sql file

---
+++ src/database/SQL/IGCSourceTables.sql
@@ -0,0 +1,21 @@
+create sequence dataLoggerID
+  start with 1
+  increment by 1
+
+create table DataLogger
+(
+    id int primary key,
+    flightDate date
+)
+
+create table IGCSourceData
+(
+   recordType char(1),
+   timeOfLog timestamp,
+   latitude int,
+   longitude int,
+   sateliteCoverage char(1),
+   pressureAltitude int,
+   GPSAltitude int,
+   fligthID primary key references DataLogger(id)
+)
