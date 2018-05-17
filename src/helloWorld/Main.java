a3d8e23d8f8bfeb1c03dc7770b96a36e5ee333ed
kenneth <254309@via.dk>
2018-05-17 09:10:13 +0200

    hello world, and oracle jdbc driver

---
+++ src/helloWorld/Main.java
@@ -0,0 +1,11 @@
+package helloWorld;
+
+/**
+ * Created by kenneth on 17/05/2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+
+    }
+}
