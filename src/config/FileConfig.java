370820da487a1bb864efe8c40b59c41013ba092a
Laegas <krystof.spiller@gmail.com>
2018-05-18 09:04:56 +0200

    Configs and Sandbox

---
+++ src/config/FileConfig.java
@@ -0,0 +1,6 @@
+package config;
+
+public class FileConfig {
+    public static final String PROJECT_PATH = "C:\\Users\\kryst\\Git\\";
+    public static final String IGC_FILE = "SEP4\\src\\resources\\SampleIgcFile.igc";
+}
--- src/test/TestLoadToSourceDB.java
+++ src/test/TestLoadToSourceDB.java
@@ -1,5 +1,6 @@
 package test;
 
+import config.FileConfig;
 import database.DAO.IGCdao;
 import fileUtil.FileDecoder;
 import model.igc.DataLogger;
@@ -7,7 +8,7 @@ import model.igc.DataLogger;
 public class TestLoadToSourceDB {
     public static void main(String args[])
     {
-        FileDecoder file = new FileDecoder(FileDecoder.FILENAME_DIRECTORY);
+        FileDecoder file = new FileDecoder(FileConfig.PROJECT_PATH + FileConfig.IGC_FILE);
         DataLogger logger = file.readFile();
 
         IGCdao dao = new IGCdao();
