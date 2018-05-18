fed364f7c6b809bac96bf15030a4c322ae892d65
Laegas <krystof.spiller@gmail.com>
2018-05-18 11:43:57 +0200

    VisualizationConfig

---
+++ src/config/VisualizationConfig.java
@@ -0,0 +1,12 @@
+package config;
+
+public class VisualizationConfig {
+    public static final double LONGITUDE_START = 8;         // 8°
+    public static final double LATITUDE_START = 54.5;       // 54°30'
+    public static final double LONGITUDE_END = 11;          // 11°
+    public static final double LATITUDE_END = 57.5;         // 57°30'
+    public static final double SOUTH_TO_NORTH_ARC = 0.5;    // 0°0'30''
+    public static final double WEST_TO_EAST_ARC = 0.25;     // 0°0'15''
+    public static final int WIDTH = (int)((LONGITUDE_END - LONGITUDE_START) * 60 / WEST_TO_EAST_ARC);   // 720
+    public static final int HEIGHT = (int)((LATITUDE_END - LATITUDE_START) * 60 / SOUTH_TO_NORTH_ARC);  // 360
+}
