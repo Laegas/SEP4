388de0cf73f77e110cd02f565879d64ebe406067
Bawon <davi-kim@hotmail.com>
2018-05-18 09:35:00 +0200

    Created DDL for weather record.

---
+++ src/database/SQL/WeatherDDL.sql
@@ -0,0 +1,34 @@
+BEGIN
+  EXECUTE IMMEDIATE 'drop table weather_record CASCADE CONSTRAINTS purge';
+  EXCEPTION
+  WHEN OTHERS THEN
+  IF SQLCODE != -942 THEN
+    RAISE;
+  END IF;
+END;
+
+BEGIN
+  EXECUTE IMMEDIATE 'drop weather_record_id';
+EXCEPTION
+  WHEN OTHERS THEN
+  IF SQLCODE != -2289 THEN
+    RAISE;
+  END IF;
+END;
+create sequence weather_record_id
+  start with 1
+  increment by 1
+  cache 100
+  nomaxvalue;
+
+create table weather_record
+(
+id int,
+ICAO_airport_code varchar(4),
+wind_direction int,
+wind_speed int,
+temperature number(3,2),
+dew_point number(3,2),
+the_date date,
+minute int
+)
