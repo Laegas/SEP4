e338c646ec7a0222d323a4ed26dfda15e95c487f
Nikolina <starfear@abv.bg>
2018-05-18 13:27:56 +0200

    Added dimension model weather ddl

---
+++ src/database/ETL/Weather/dimensional_model_weather_ddl.sql
@@ -0,0 +1,27 @@
+BEGIN
+  EXECUTE IMMEDIATE 'drop table F_WEATHER_RECORD CASCADE CONSTRAINTS purge';
+  EXCEPTION
+  WHEN OTHERS THEN
+  IF SQLCODE != -942 THEN
+    RAISE;
+  END IF;
+END;
+/
+
+
+CREATE table F_WEATHER_RECORD(
+  surr_key_weather INT,
+  surr_key_flight INT REFERENCES D_FLIGHT(surr_key_flight),
+
+  date date;
+  time TIMESTAMP,
+  wind_direction char(4),
+  wind_speed int ,
+  temperature decimal (3,2),
+  dew_point decimal (3,2),
+  airport_code varchar(4),
+
+  PRIMARY KEY (surr_key_weather,surr_key_flight)
+);
+
+COMMIT;
