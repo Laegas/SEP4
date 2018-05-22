ff401b40ca16d331565b8a4075fa90dc0ff53e3d
Laegas <krystof.spiller@gmail.com>
2018-05-22 15:49:01 +0200

    RuntimeException because Kenneth

--- src/model/geography/InvalidCoordinatesException.java
+++ src/model/geography/InvalidCoordinatesException.java
@@ -1,6 +1,6 @@
 package model.geography;
 
-public class InvalidCoordinatesException extends Exception {
+public class InvalidCoordinatesException extends RuntimeException {
     public InvalidCoordinatesException(String message) {
         super(message);
     }
