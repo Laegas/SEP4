e8dc47d5e07fe3c2fde4774a34b4f810dc4d8f6d
kenneth <254309@via.dk>
2018-05-23 13:24:06 +0200

    wonnaPullFromKrystof

--- src/config/DatabaseConfig.java
+++ src/config/DatabaseConfig.java
@@ -1,7 +1,7 @@
 package config;
 
 public class DatabaseConfig {
-    public static final String USERNAME = "Laegas";
-    public static final String PASSWORD = "1234";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "password";
     public static final String CONNECTION_STRING = "jdbc:oracle:thin:@localhost:1521:orcl";
 }
