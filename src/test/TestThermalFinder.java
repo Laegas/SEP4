1612a10dbe724d0742b880c89c35df321364aa9d
Bawon <davi-kim@hotmail.com>
2018-05-23 15:44:52 +0200

    ThermalFinder partly done.

---
+++ src/test/TestThermalFinder.java
@@ -0,0 +1,32 @@
+package test;
+
+import fileUtil.FileDecoder;
+import model.igc.Flight;
+import model.igc.ThermalDataPointGroup;
+import util.thermal.ThermalFinder;
+import util.thermal.ThermalFinderImp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestThermalFinder {
+    public static void main(String[] args)
+    {
+        FileDecoder fd = new FileDecoder("C:\\igc.\\85C_2B.igc");
+        Flight flight = fd.readFile();
+        System.out.println(flight.getDatalog().size());
+
+        ThermalFinder finder = new ThermalFinderImp();
+        List<ThermalDataPointGroup> groups = finder.findThermalsUsingPressureAltitude(flight);
+
+        System.out.println(groups.size() +" amount of thermals");
+        for(int i = 0;i<groups.size();i++)
+        {
+            System.out.println(  groups.get(i).getGroup().size()+" amount of datapoints in thermal");
+            ThermalDataPointGroup a = groups.get(i);
+            System.out.println(a.getGroup().get(0).getPressureAltitude()+"   to    "+ a.getGroup().get(a.getGroup().size()-1).getPressureAltitude());
+        }
+
+
+    }
+}
