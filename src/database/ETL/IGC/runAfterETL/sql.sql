aa634193f6f0e1e1ede02e0de557727451b76497
Laegas <krystof.spiller@gmail.com>
2018-05-29 13:32:50 +0200

    Name change

--- src/database/ETL/IGC/runAfterETL/sql.sql
+++ src/database/ETL/IGC/runAfterETL/sql.sql
@@ -1,2 +1,2 @@
 delete from IGC_SOURCE_DATA where id in (select igc_id from FULLY_EXTRACTED_IGC);
-DELETE from data_logger where id in (select flight_id FROM FULLY_EXTRACTED_IGC);
+DELETE from data_logger_source where id in (select flight_id FROM FULLY_EXTRACTED_IGC);
