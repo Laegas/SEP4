854d07fe46a8082cc1d9b525892631529e26ee73
Laegas <krystof.spiller@gmail.com>
2018-05-31 15:56:39 +0200

    Day's worth of work commit, enjoy. Let the Git God deliver this commit with care.

--- src/visualization/javaFxMaps/settings.js
+++ src/visualization/javaFxMaps/settings.js
@@ -6,11 +6,12 @@ var longitude = 9.5;
 var zoom = 7;
 
 // layer settings
-var minimumAlpha = [0.3, 0.3]
-var alphaProperty = ["t", "t"]
-var alphaDividerProperty = [6.0, 6.0]
-var visibilityFormula = ["t/f", "t/f"]
-var displayStringFormat = ["T:@2 / F:@1", "T:@2 / F:@1"]
-var fromColor = ["255, 0, 0", "255, 0, 0"]
-var toColor = ["255, 0, 0", "255, 0, 0"]
+var layerName = ["All", "All2"];
+var minimumAlpha = [0.3, 0.3];
+var alphaProperty = ["t", "t"];
+var alphaDividerProperty = [0.0, 0.0];
+var visibilityFormula = ["t/f", "t/f"];
+var displayStringFormat = ["T:@2 / F:@1", "T:@2 / F:@1"];
+var fromColor = [[255, 0, 0], [0, 255, 0]];
+var toColor = [[255, 0, 0], [255, 0, 0]];
 var properties = ["f", "t"];
